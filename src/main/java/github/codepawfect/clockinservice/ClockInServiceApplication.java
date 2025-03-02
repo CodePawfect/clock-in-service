35cf82efa7cbba0af6d01138ceab23ec1eb2480d
CodePawfect <codepawfect@gmail.com>
2025-03-02 12:09:25 +0100

    Add Mongock integration for database migrations and initialize users collection

--- src/main/java/github/codepawfect/clockinservice/ClockInServiceApplication.java
+++ src/main/java/github/codepawfect/clockinservice/ClockInServiceApplication.java
@@ -1,9 +1,11 @@
 package github.codepawfect.clockinservice;
 
+import io.mongock.runner.springboot.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class ClockInServiceApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/github/codepawfect/clockinservice/migration/UsersCollectionInitialization.java
@@ -0,0 +1,61 @@
+package github.codepawfect.clockinservice.migration;
+
+import io.mongock.api.annotations.ChangeUnit;
+import io.mongock.api.annotations.Execution;
+import io.mongock.api.annotations.RollbackExecution;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.index.Index;
+import org.springframework.data.mongodb.core.index.IndexDefinition;
+import org.springframework.data.mongodb.core.index.IndexOperations;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Initializes the users collection in the database.
+ */
+@ChangeUnit(id = "users-collection-initialization", order = "001", author = "codepawfect")
+public class UsersCollectionInitialization {
+
+    /**
+     * Creates the users collection and indexes.
+     *
+     * @param mongoTemplate The MongoTemplate to use for database operations.
+     */
+    @Execution
+    public void execution(MongoTemplate mongoTemplate) {
+        if (!mongoTemplate.collectionExists("users")) {
+            mongoTemplate.createCollection("users");
+
+            IndexOperations indexOps = mongoTemplate.indexOps("users");
+
+            IndexDefinition usernameIndex = new Index()
+                    .on("username", Sort.Direction.ASC)
+                    .unique();
+
+            indexOps.ensureIndex(usernameIndex);
+
+            IndexDefinition emailIndex = new Index()
+                    .on("email", Sort.Direction.ASC)
+                    .unique();
+
+            indexOps.ensureIndex(emailIndex);
+
+            IndexDefinition lastLoginIndex = new Index()
+                    .on("lastLogin", Sort.Direction.DESC)
+                    .expire(90, TimeUnit.DAYS);
+
+            indexOps.ensureIndex(lastLoginIndex);
+        }
+    }
+
+    /**
+     * Drops the users collection.
+     *
+     * @param mongoTemplate The MongoTemplate to use for database operations.
+     */
+    @RollbackExecution
+    public void rollbackExecution(MongoTemplate mongoTemplate) {
+        mongoTemplate.dropCollection("users");
+    }
+}
+
