bd731f6e7508a27d0ae522e122b02876b0ca7a2d
CodePawfect <codepawfect@gmail.com>
2025-03-03 15:35:13 +0100

    Add integration test setup and MongoDB configuration; create base test class and initial controller tests

---
+++ src/test/java/github/codepawfect/clockinservice/adapter/worktime/in/WorkTimeControllerTest.java
@@ -0,0 +1,20 @@
+package github.codepawfect.clockinservice.adapter.worktime.in;
+
+import github.codepawfect.clockinservice.BaseIntegrationTest;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * WorkTimeControllerTest is a test class for the WorkTimeController.
+ */
+class WorkTimeControllerTest extends BaseIntegrationTest {
+
+    @Test
+    void createWorkTime() {
+    }
+
+    @Test
+    void getWorkTimes() {
+    }
+}
